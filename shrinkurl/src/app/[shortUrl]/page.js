a3873d9aed181563e73b67540b252ff1449d9b73
Arjit Chauhan <arjitchauhan76@gmail.com>
2025-08-22 15:14:11 +0530

    Migrated [shortUrl] page to JavaScript

--- shrinkurl/src/app/[shortUrl]/page.js
+++ shrinkurl/src/app/[shortUrl]/page.js
@@ -1,15 +1,10 @@
+// src/app/[shortUrl]/page.js
+
 import { redirect } from 'next/navigation';
 import { getUrl } from '../../../db/queries';
 
-
-interface PageProps {
-  params: {
-    shortUrl: string;
-  };
-}
-
-export default async function RedirectPage({ params }: PageProps) {
-  const { shortUrl } = params;
+export default async function RedirectPage({ params }) {
+  const shortUrl = params.shortUrl;
 
   const longUrl = await getUrl(shortUrl);
 
