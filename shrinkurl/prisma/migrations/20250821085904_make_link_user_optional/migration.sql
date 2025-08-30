884fb1b6a3d1a6551dc46bb266595c4f35a78848
Arjit Chauhan <126594119+Arjit8807@users.noreply.github.com>
2025-08-30 22:38:59 +0530

    Delete unnecessary directory

--- shrinkurl/prisma/migrations/20250821085904_make_link_user_optional/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Link" ALTER COLUMN "userId" DROP NOT NULL;
--- shrinkurl/src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/db.ts
-// This file is a shared utility for your backend.
-
-// We declare a global Map to store user data.
-// `export` makes this Map accessible to other files.
-// `const` ensures that this Map variable cannot be reassigned.
-export const users = new Map();
